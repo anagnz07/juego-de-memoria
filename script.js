4d2f8f424f70f746f93c85160707066dca9e5424
pferrandiz <95432114+pferrandiz@users.noreply.github.com>
2022-03-18 11:02:43 +0100

    feat: feat/añadir estilos al juego

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 "use strict";
-const emojis = [" 👹", " 💀", " 👻", " 👽", " 🤖", " 💣", " 💩", " 🌵"];
+const emojis = [" 💊", " 💀", " 👻", " 👽", " 🤖", " 💣", " 🍺", " 🪰"];
 const main = document.querySelector("main");
 
 ///Generar cartas aleatoriamente
 function generarNumeroAleatorio(max) {
-    const min = 0;
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1) + min);
+  const min = 0;
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 function generarCartasRandom(arr) {
-    let veces = 0;
-    do {
-        const myArr = [...arr];
-        for (let i = myArr.length - 1; i >= 0; i--) {
-            let numeroRandom = generarNumeroAleatorio(myArr.length - 1);
-            let node = document.createElement("section");
-            node.className = "card";
-            node.innerHTML = `<div class="content"><div class="front"> #>/<> </div><div class="back">${myArr[numeroRandom]}</div></div>`;
-            main.appendChild(node);
-            myArr.splice(numeroRandom, 1);
-        }
-        veces++;
-    } while (veces <= 1);
+  let veces = 0;
+  do {
+    const myArr = [...arr];
+    for (let i = myArr.length - 1; i >= 0; i--) {
+      let numeroRandom = generarNumeroAleatorio(myArr.length - 1);
+      let node = document.createElement("section");
+      node.className = "card";
+      node.innerHTML = `<div class="content"><div class="front"></div><div class="back">${myArr[numeroRandom]}</div></div>`;
+      main.appendChild(node);
+      myArr.splice(numeroRandom, 1);
+    }
+    veces++;
+  } while (veces <= 1);
 }
 
 generarCartasRandom(emojis);
@@ -38,38 +38,38 @@ let textoPrimeraCarta;
 let textoSegundaCarta;
 
 const reveal = (e) => {
-    if (num < 2) {
-        const currentCard = e.currentTarget;
-        const textCard = currentCard.childNodes[0].childNodes[1].textContent;
+  if (num < 2) {
+    const currentCard = e.currentTarget;
+    const textCard = currentCard.childNodes[0].childNodes[1].textContent;
 
-        //Comprobamos que la carta seleccionada no este girada
-        if (currentCard.className != "card flipped") {
-            currentCard.classList.add("flipped");
-            if (num < 1) {
-                primeraCarta = currentCard;
-                textoPrimeraCarta = textCard;
-            } else {
-                segundaCarta = currentCard;
-                textoSegundaCarta = textCard;
-            }
-            num += 1;
-            if (num == 2) {
-                setTimeout(() => {
-                    if (textoPrimeraCarta != textoSegundaCarta) {
-                        primeraCarta.classList.remove("flipped");
-                        segundaCarta.classList.remove("flipped");
-                    }
-                    num = 0;
-                }, 500);
-                count++;
-                contador.textContent = count;
-            }
-        }
+    //Comprobamos que la carta seleccionada no este girada
+    if (currentCard.className != "card flipped") {
+      currentCard.classList.add("flipped");
+      if (num < 1) {
+        primeraCarta = currentCard;
+        textoPrimeraCarta = textCard;
+      } else {
+        segundaCarta = currentCard;
+        textoSegundaCarta = textCard;
+      }
+      num += 1;
+      if (num == 2) {
+        setTimeout(() => {
+          if (textoPrimeraCarta != textoSegundaCarta) {
+            primeraCarta.classList.remove("flipped");
+            segundaCarta.classList.remove("flipped");
+          }
+          num = 0;
+        }, 500);
+        count++;
+        contador.textContent = count;
+      }
     }
+  }
 };
 
 for (const card of cards) {
-    card.addEventListener("click", reveal);
+  card.addEventListener("click", reveal);
 }
 
 /**
@@ -81,6 +81,6 @@ const buttonStart = document.querySelector(".buttonStart");
 const menuPrincipal = document.querySelector(".menuPrincipal");
 
 buttonStart.addEventListener("click", function (e) {
-    e.preventDefault();
-    menuPrincipal.style.visibility = "hidden";
+  e.preventDefault();
+  menuPrincipal.style.visibility = "hidden";
 });
